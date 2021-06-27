f47767726e7a08670d5a3b9ea2089a9321677170
saadi299 <saadimohammed149@gmail.com>
2021-06-27 21:32:57 +0600

    Start Building Expense Tracker

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,8 @@
+import "./ExpenseItem.css";
+
+function ExpenseItem() {
+  return (
+    <div className="expense-item">
+      <div>March 28th 2021</div>
+      <div className="expense-item__description"> 
+        <h2>Car Insu
