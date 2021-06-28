427551880846841bc488728732c6b7b70969f200
saadi299 <saadimohammed149@gmail.com>
2021-06-29 00:08:09 +0600

    Expense Section Complete

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,16 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+  
   return (
     <div className="expense-item">
-      <div>March 28th 2021</div>
+      <ExpenseDate date={props.date}/>
       <div className="expense-item__description"> 
-        <h2>Car Insu
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">$ {props.amount}</div>
+      </div>
+    </div>
+  );
+}
+export default ExpenseItem;
