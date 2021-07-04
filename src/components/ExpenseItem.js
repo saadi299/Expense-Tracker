e7837d63f9d8d37fefeac6684efcbcbe63dc1fc9
saadi299 <saadimohammed149@gmail.com>
2021-07-04 14:09:37 +0600

    Composition and Use state used

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,26 @@
+import React, { useState } from 'react';
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 
+
 function ExpenseItem(props) {
   
+  const [title,setTitle]=useState(props.title);
+
+  // let title=props.title;
+  const clickHandler= () =>{
+    setTitle('update');
+  };
+ 
+
   return (
     <div className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description"> 
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">$ {props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Update Title</button>
     </div>
   );
 }
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,9 +1,10 @@
+import './Expenses.css';
 import ExpenseItem from "./ExpenseItem";
-function Expenses(props){
-
-    return(
-        <div>
-             <ExpenseItem
+import Card from "./Card";
+function Expenses(props) {
+  return (
+    <Card className="expenses">
+      <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
         date={props.items[0].date}
@@ -22,8 +23,8 @@ function Expenses(props){
         title={props.items[3].title}
         amount={props.items[3].amount}
         date={props.items[3].date}
-      ></ExpenseItem> 
-        </div>
-    );
+      ></ExpenseItem>{" "}
+    </Card>
+  );
 }
 export default Expenses;
